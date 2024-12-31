cb4d1427d1c5ff214032d6f97f6598b3e6d4f39c
NayakBits <65670718+SweekrithiNayyyak@users.noreply.github.com>
2024-12-31 16:05:36 +0530

    Create haloween_sale.py

---
+++ Implementation/haloween_sale.py
@@ -0,0 +1,52 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'howManyGames' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER p
+#  2. INTEGER d
+#  3. INTEGER m
+#  4. INTEGER s
+#
+
+def howManyGames(p, d, m, s):
+    num_of_games = 0
+    price = p
+    total_spent = 0
+
+    while total_spent + price <= s:
+        total_spent += price
+        num_of_games += 1
+        price = max(price - d, m)  # Reduce the price but ensure it doesn't go below `m`
+    
+    return num_of_games
+            
+    
+    # Return the number of games you can buy
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    p = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    m = int(first_multiple_input[2])
+
+    s = int(first_multiple_input[3])
+
+    answer = howManyGames(p, d, m, s)
+
+    fptr.write(str(answer) + '\n')
+
+    fptr.close()
